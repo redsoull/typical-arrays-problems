bd2b0ae8652716887225fdc09473da475239c5d4
Volha Koval <volha.koval@mapbox.com>
2021-08-23 19:30:59 +0300

    init: update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
-exports.min = function min (array) {
+exports.min = function min (array = [0]) {
+
+if (array.length !== 0) {
+  return Math.min.apply(null, array);
+}
   return 0;
+};
+
+exports.max = function max (array = [0]) {
+  if (array.length !== 0) {
+    return Math.max.apply(null, array);
+  }
+    return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.avg = function avg (array = [0]) {
+if (array.length !== 0) {
+  let sum = 0;
+  for (i = 0; i < array.length; i +=1) {
+    sum = sum + array[i];
+  }
+  return sum / array.length;
 }
 
-exports.avg = function avg (array) {
   return 0;
 }
